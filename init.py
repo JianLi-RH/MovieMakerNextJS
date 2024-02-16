8d08f922a948b72cd3f72309f0cd2de294aa64f5
Jian Li <jianl@redhat.com>
2024-02-16 12:05:41 +0800

    更新Dockerfile和init.py

--- init.py
+++ init.py
@@ -3,8 +3,13 @@ import sqlite3
 def CreateDatabase():
     con = sqlite3.connect("moviemaker.db")
     cur = con.cursor()
-    cur.execute("CREATE TABLE users (guid TEXT NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL)")
-    cur.execute("CREATE TABLE login_status (guid TEXT NOT NULL, username TEXT NOT NULL, timestamp INT NOT NULL)")
+
+    cur.execute("SELECT 1 FROM sqlite_master WHERE type='table' AND name=?", ('users',))
+    result = cur.fetchone()
+    print(result)
+    if not result:
+        cur.execute("CREATE TABLE users (guid TEXT NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL)")
+        cur.execute("CREATE TABLE login_status (guid TEXT NOT NULL, username TEXT NOT NULL, timestamp INT NOT NULL)")
 
     con.close()
 
